7c84f56277926238aa1f1fe26dc585f298ff8415
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-20 23:00:09 +0800

    [react-moviescombine][Koby] feat: click on swipercard to dummy page and return from dummy page

--- src/components/MainPageComponents/MovieSwiper.js
+++ src/components/MainPageComponents/MovieSwiper.js
@@ -3,21 +3,23 @@ import { Swiper } from "antd-mobile";
 
 import "../../style/MainPage.css";
 import { useSelector } from "react-redux";
+import { useHistory } from 'react-router-dom';
 
 function MovieSwiper(props) {
+  const history = useHistory();
   const showingMovieList = useSelector((state) => state.showingMovieList);
   const upcomingMovieList = useSelector((state) => state.upcomingMovieList);
 
   function getMovieData(movieList) {
     if (movieList != null) {
-      return movieList.map((movie, index) => (
+      return movieList.slice(0, 3).map((movie, index) => (
         <Swiper.Item key={index}>
           <div
             className="content"
             style={{
               backgroundImage: "url(" + movie.posterSource + ")",
             }}
-            onClick={() => {}}
+            onClick={() => history.push('/MovieDetails')}
           ></div>
         </Swiper.Item>
       ));
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,59 +1,62 @@
 import { Rate } from "antd-mobile";
+import { LeftOutline } from 'antd-mobile-icons'
 import "../style/MovieDetails.css";
+import { useHistory } from "react-router-dom";
 import YoutubeEmbed from "./YoutubeEmbed";
-function MovieDetails({
-  title,
-  trailerSource,
-  description,
-  genre,
-  category,
-  rating,
-}) {
-    const imgurl = "https://pro2-bar-s3-cdn-cf2.myportfolio.com/f44a6a86-6cdb-4ac5-b205-003353cc5a9c/3297b6ad-5b32-4e86-96c5-f82c5275fbe8_rw_1200.jpg?h=4fb0a82075181bfcbafbadfe3291acec"
-    const sectionStyle = {
-        background: " linear-gradient(to top, rgba(0, 0, 0, 255) 25%, rgb(0, 0, 0,0) ),url("+imgurl+"})",
-        borderRadius : "50px",
-        backgroundSize: "contain",
-      }
-      
+function MovieDetails(props) {
+  console.log(props.posterSource);
+  const history = useHistory();
+  const imgurl =
+    "https://pro2-bar-s3-cdn-cf2.myportfolio.com/f44a6a86-6cdb-4ac5-b205-003353cc5a9c/3297b6ad-5b32-4e86-96c5-f82c5275fbe8_rw_1200.jpg?h=4fb0a82075181bfcbafbadfe3291acec";
+  const sectionStyle = {
+    background:
+      " linear-gradient(to top, rgba(0, 0, 0, 255) 25%, rgb(0, 0, 0,0) ),url(" +
+      imgurl +
+      "})",
+    borderRadius: "50px",
+    backgroundSize: "contain",
+  };
+
   return (
-    <div style={ sectionStyle }>
-        <div className="container">
-        <div className="emptyDiv"/>
+    <>
+    <div className="backText" onClick={() => history.goBack()}><LeftOutline className="backArrow"/>Movie Details</div>
+    <div style={sectionStyle}>
+      <div className="container">
+        <div className="emptyDiv" />
         <div className="movieName">Spider-Man: No Way Home</div>
         <div className="ratingGenreFlex">
-            <spam className="genre">Drama</spam>
-            <Rate className="star" allowHalf readOnly value={4.5} />
+          <spam className="genre">Drama</spam>
+          <Rate className="star" allowHalf readOnly value={4.5} />
         </div>
         <div className="otherDetailsFlex">
-            <div className="otherDetails">
-                <div className="otherDetailsTitle">Duration</div>
-                <div className="otherDetailsValue">148 mins</div>
-            </div>
-            <div className="otherDetails">
-                <div className="otherDetailsTitle">Release</div>
-                <div className="otherDetailsValue">15 Dec 2021</div>
-            </div>
-            <div className="otherDetails">
-                <div className="otherDetailsTitle">Category</div>
-                <div className="otherDetailsValue">IIB</div>
-            </div>
+          <div className="otherDetails">
+            <div className="otherDetailsTitle">Duration</div>
+            <div className="otherDetailsValue">148 mins</div>
+          </div>
+          <div className="otherDetails">
+            <div className="otherDetailsTitle">Release</div>
+            <div className="otherDetailsValue">15 Dec 2021</div>
+          </div>
+          <div className="otherDetails">
+            <div className="otherDetailsTitle">Category</div>
+            <div className="otherDetailsValue">IIB</div>
+          </div>
         </div>
         <div id="movieDescription">
-            <p>
+          <p>
             For the first time in the cinematic history of Spider-Man, our
-            friendly neighborhood hero is unmasked and no longer able to separate
-            his normal life from the high-stakes of being a Super Hero. When he
-            asks for help from Doctor Strange the stakes become even more
-            dangerous, forcing him to discover what it truly means to be
+            friendly neighborhood hero is unmasked and no longer able to
+            separate his normal life from the high-stakes of being a Super Hero.
+            When he asks for help from Doctor Strange the stakes become even
+            more dangerous, forcing him to discover what it truly means to be
             Spider-Man.
-            </p>
+          </p>
         </div>
         <div className="trailer">Trailer</div>
-        <YoutubeEmbed embedId="rt-2cxAiPJk"/>
-        </div>        
+        <YoutubeEmbed embedId="rt-2cxAiPJk" />
+      </div>
     </div>
-
+    </>
   );
 }
 export default MovieDetails;
