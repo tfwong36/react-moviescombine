2b5b956a9a7c14499bcd847a248fd982a9fa4f5d
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-20 10:14:09 +0800

    [movieCombine-react][Anna] feat: installed antD icon + react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <nav className="navigation">
+          <ul>
+              <Link to="/" className="navigation-tab">ToDoList🕒</Link>
+              <Link to="/Done" className="navigation-tab">Done✔️</Link>
+              <Link to="/UnDone" className="navigation-tab">UnDone❌</Link>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/Done">
+          </Route>
+          <Route exact path="/UnDone">
+          </Route>
+          <Route exact path="/">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
