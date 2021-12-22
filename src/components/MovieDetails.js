5c11c0401746c2ca3c9566ac890e3d2a53c166f5
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-22 19:21:11 +0800

    [movieCombine-react][Anna] feat: merge conflict

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,14 +3,24 @@ import { NavBar } from "antd-mobile";
 import { Button } from "antd-mobile";
 import { LeftOutline } from "antd-mobile-icons";
 import { useEffect } from "react";
+import { Dialog } from "antd-mobile-icons";
 import "../style/MovieDetails.css";
 import { useHistory, useLocation } from "react-router-dom";
 import YoutubeEmbed from "./YoutubeEmbed";
 function MovieDetails() {
   const location = useLocation();
   const history = useHistory();
-
+  const {
     id,
+    category,
+    description,
+    genre,
+    posterSource,
+    rating,
+    title,
+    releaseDate,
+    trailerSource,
+  } = location.state;
   const sectionStyle = {
     background:
       " linear-gradient(to top, rgba(0, 0, 0, 255) 25%, rgb(0, 0, 0,0) ),url(" +
