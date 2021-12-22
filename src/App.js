26d5a86b1bfb014977e6ab7398afc757d759d95d
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-22 19:33:36 +0800

    [react-moviescombine]fix: fix route bug

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ function App() {
               <Link to="/" className="navigation-tab">
                 <SearchOutline color="white" fontSize={36} />
               </Link>
-              <Link to="/selectSeat" className="navigation-tab">
+              <Link to="/MyTickets" className="navigation-tab">
                 <CouponOutline color="white" fontSize={36} />
               </Link>
             </ul>
           </nav>
           <Switch>
-            <Route exact path="/selectSeat">
+            <Route exact path="/MyTickets">
               <MyTickets></MyTickets>
             </Route>
             <Route exact path="/MovieDetails">
@@ -44,6 +44,9 @@ function App() {
             </Route>
             <Route exact path="/Payment">
               <Payment></Payment>
+            </Route>
+            <Route exact path="/SelectSeat">
+              <SelectSeat></SelectSeat>
             </Route>
             <Route exact path="/">
               <MainPage></MainPage>
