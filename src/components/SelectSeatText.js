cdec847bf69d7ed7751cd6ed7487bbb88a95e66b
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-22 09:35:27 +0800

    [react-moviescombine][Koby] feat: add whitespace above purchase button

--- src/components/SelectSeatText.js
+++ src/components/SelectSeatText.js
@@ -3,7 +3,7 @@ function SelectSeatText ({selectedSeats}) {
     if (selectedSeats.length > 0){
         return <dir className="selected-seat-text"> Selected Seat {selectedSeats.join(',')}.</dir>
     }
-    else  return <dir></dir>
+    else  return <dir className="selected-seat-text">&nbsp;</dir>
 }
 
 export default SelectSeatText;
