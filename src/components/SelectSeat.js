d657a55a45fdaa7d2705255fef55e4f6c19d9d0c
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-22 10:42:54 +0800

    [react-moviescombine][Koby] feat: Add way to pass info to purchase

--- src/components/SelectSeat.js
+++ src/components/SelectSeat.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom";
 import { Button } from 'antd-mobile'
 import "../style/SelectSeat.css";
 import SeatingMap from "./SeatingMap";
@@ -10,7 +10,21 @@ import SeatAvailability from "./SeatAvailability"
 
 function SelectSeat(){
     const [selectedSeats, setSelectedSeats] = useState([]);
+    // let location = useLocation();
     const history = useHistory();
+    // const {
+    //   category,
+    //   description,
+    //   genre,
+    //   title,
+    //   price,
+    //   releaseDate,
+    //   trailerSource,
+    // } = location.state;
+    const title = 'spider-man'
+    const price = 123.4
+    const cinemaDetail = 'Emperor Cinemas (Ma On Shan)';
+    const showDateandTime = '22 Dec 2021 (Wed) 15:10';
     const toggleSeatSelect = (seat) =>{
         if (!selectedSeats.includes(seat)){
             setSelectedSeats([...selectedSeats ,seat])
@@ -23,16 +37,16 @@ function SelectSeat(){
     return (
         <>
             <div className="container">
-                <div className="movieTitle">Spider-Man No Way Home</div>
-                <div className="priceDuration">Price: $123| Duration: 128.3 minures</div>
-                <div className="cinemaDetail">Emperor Cinemas (Ma On Shan)</div>
-                <div className="showDateandTime">22 Dec 2021 (Wed) 15:10</div>
+                <div className="movieTitle">{title}</div>
+                <div className="priceDuration">Price: ${price}| Duration: 128.3 minures</div>
+                <div className="cinemaDetail"></div>
+                <div className="showDateandTime">{showDateandTime}</div>
             </div>
             <div className="seating-map-container">
                 <SeatingMap toggleSeatSelect={toggleSeatSelect}/>    
                 <SeatAvailability/> 
                 <SelectSeatText selectedSeats={selectedSeats}/>    
-                <Button className='seating-map-purchase-btn' onClick={() => history.push("/Payment", selectedSeats)}>
+                <Button className='seating-map-purchase-btn' onClick={() => history.push("/Payment",{selectedSeats,cinemaDetail,title,price,showDateandTime})}>
                   PURCHASE
                 </Button> 
             </div>
